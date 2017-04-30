8b1f2f7b33f5bb3ff1e5fb79040cdedc0d3fb4c9
John Furtado <jf8846@gmail.com>
2017-04-30 16:30:01 -0400

    displays list of games from the api

--- app.js
+++ app.js
@@ -1,19 +1,100 @@
 
-$(document).ready(function() {
+var model = {
+  playlistItems: [],
+  browseItems: []
+}
 
-  // send off the query
-  $.ajax ({
-    type: 'GET',
-    dataType: 'jsonp',
-    crossDomain: true,
-    jsonp: 'json_callback',
-    url: 'http://www.giantbomb.com/api/search/?format=jsonp&api_key=555a89bb8e2e7057011cef979452373c3100ed82&query=Witcher'
-}).done(function(data) {
-    console.log(data.results[1].name);
-}).fail(function() {
-  console.log(data)
-  alert("error");
-}).always(function() {
-  alert("complete");
-});
+function discoverMovies(callback) {
+
+  // ask the API for games related to the keywords that are hardcoded into the URL
+  $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      crossDomain: true,
+      jsonp: 'json_callback',
+      url: 'http://www.giantbomb.com/api/search',
+      data: {
+        format: "jsonp",
+        api_key: "",
+        query: "Witcher"
+
+      },
+    success: function(data) {
+      console.log(data.results)
+      model.browseItems = data.results;
+      callback(data);
+    }
+  });
+}
+
+function render() {
+
+  // clear everything
+  $("#section-watchlist ul").empty();
+  $("#section-browse ul").empty();
+
+  // render watchlist items
+  // model.watchlistItems.forEach(function(movie) {
+  //   var title = $("<h6></h6>").text(movie.original_title);
+  //
+  //   // movie poster
+  //   var poster = $("<img></img>")
+  //     .attr("src", api.posterUrl(movie))
+  //     .attr("class", "img-responsive");
+  //
+  //   // "I watched it" button
+  //   var button = $("<button></button>")
+  //     .text("I watched it")
+  //     .attr("class", "btn btn-danger")
+  //     .click(function() {
+  //       var index = model.watchlistItems.indexOf(movie);
+  //       model.watchlistItems.splice(index, 1);
+  //       render();
+  //     });
+  //
+  //   // panel heading contains the title
+  //   var panelHeading = $("<div></div>")
+  //     .attr("class", "panel-heading")
+  //     .append(title);
+  //
+  //   // panel body contains the poster and button
+  //   var panelBody = $("<div></div>")
+  //     .attr("class", "panel-body")
+  //     .append( [poster, button] );
+  //
+  //   // list item is a panel, contains the panel heading and body
+  //   var itemView = $("<li></li>")
+  //     .append( [panelHeading, panelBody] )
+  //     .attr("class", "panel panel-default");
+  //
+  //   $("#section-watchlist ul").append(itemView);
+  // });
+
+  // render browse items
+  model.browseItems.forEach(function(game) {
+    var title = $("<h4></h4>").text(game.name);
+    var overview = $("<p></p>").text(game.deck);
+
+    // button for adding to watchlist
+    var button = $("<button></button>")
+      .text("Add to Playlist")
+      .attr("class", "btn btn-primary")
+      .click(function() {
+        model.playlistItems.push(game);
+        render();
+      })
+      // .prop("disabled", model.watchlistItems.indexOf(movie) !== -1)
+      .attr("class", "btn btn-primary");
+
+    var itemView = $("<li></li>")
+      .attr("class", "list-group-item")
+      .append( [title, overview, button] );
+
+    // append the itemView to the list
+    $("#section-browse ul").append(itemView);
+  });
+}
+
+$(document).ready(function() {
+  discoverMovies(render);
 });
